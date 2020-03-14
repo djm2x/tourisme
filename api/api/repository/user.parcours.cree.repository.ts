5e1a19e2fd92126382d9ab2161695c31d6857e7b
djm2x <dj-m2x@hotmail.com>
2020-03-14 01:11:32 +0100

    add back and front

--- api/api/repository/user.parcours.cree.repository.ts
+++
@@ -1,9 +0,0 @@
-import { SuperRepository } from './super.repository';
-import { UserParcoursCree } from '../model/models';
-
-export class UserParcoursCreeRepository extends SuperRepository<UserParcoursCree> {
-
-  constructor() {
-    super(UserParcoursCree);
-  }
-}
