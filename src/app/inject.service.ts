2967a1c6a5e1c18e1b5ce6973512eed7acb60202
djm2x <dj-m2x@hotmail.com>
2020-03-14 18:17:33 +0100

    clear

--- src/app/inject.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, Injector, InjectionToken } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class InjectService {
-  static injector: Injector;
-  // static injector: InjectionToken<any>;
-}
